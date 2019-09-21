9a5c732ae65115921b0960dcfe0285002ac384e6
kanin <hattakul99@gmail.com>
2019-09-21 10:41:19 +0300

    H03

--- offline.js
+++ offline.js
@@ -1,11 +1,11 @@
 ﻿{
-	"version": 1569016069,
+	"version": 1569051532,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
+		"images/bg-sheet0.png",
 		"images/arrow1-sheet0.png",
 		"images/sprite2-sheet0.png",
 		"images/sprite3-sheet0.png",
@@ -16,6 +16,8 @@
 		"images/sprite6-sheet0.png",
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
+		"images/p01-sheet0.png",
+		"images/sprite10-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
